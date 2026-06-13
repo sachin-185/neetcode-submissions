f99214ea6c9c87b409765305298bd2037e096938
SACHIN S <svsachin2005@gmail.com>
2026-06-13 09:45:10 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n + 1][2];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][0] = Math.max(dp[i + 1][0], -prices[i] + dp[i + 1][1]);
+            dp[i][1] = Math.max(dp[i + 1][1], prices[i] + dp[i + 1][0]);
+        }
+        return dp[0][0];
+    }
+}
