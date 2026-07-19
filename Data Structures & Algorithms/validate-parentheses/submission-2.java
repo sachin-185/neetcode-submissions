6b5c76fc4934621926823e8f22b618179edf1d07
SACHIN S <svsachin2005@gmail.com>
2026-07-19 09:40:27 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        Map<Character,Character> m = new HashMap<>();
+        m.put(')','(');
+        m.put(']','[');
+        m.put('}','{');
+
+        for(char i : s.toCharArray()){
+            if(m.containsKey(i)){
+                if(!st.isEmpty() && st.peek() == m.get(i)) st.pop();
+                else return false;
+            }
+            else st.push(i);
+        }
+        return st.isEmpty();
+    }
+}
