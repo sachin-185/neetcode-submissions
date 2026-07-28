6a222b2d5fe1fefd0f79dc18c4c2660bb3bde02c
SACHIN S <svsachin2005@gmail.com>
2026-07-28 23:47:03 +0530

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.add(p); q2.add(q);
+        while(!q1.isEmpty() && !q2.isEmpty()){
+                for (int i = q1.size(); i > 0; i--){
+                        TreeNode nodeP = q1.poll();
+                        TreeNode nodeQ = q2.poll();
+                        if (nodeP == null && nodeQ == null) continue;
+                        if(nodeP == null || nodeQ == null || nodeP.val != nodeQ.val) return false;
+                        q1.add(nodeP.left);
+                        q1.add(nodeP.right);
+                        q2.add(nodeQ.left);
+                        q2.add(nodeQ.right);
+                }
+        }
+        return true;
+    }
+}
