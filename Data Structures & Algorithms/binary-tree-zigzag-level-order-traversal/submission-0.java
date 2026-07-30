b622edbc0da79b6e9f6250e66c8f06be2f44f36f
SACHIN S <svsachin2005@gmail.com>
2026-07-31 00:48:00 +0530

    Add: binary-tree-zigzag-level-order-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-zigzag-level-order-traversal/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            Integer[] level = new Integer[size];
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                int idx = (res.size() % 2 == 0) ? i : size - i - 1;
+                level[idx] = node.val;
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            res.add(Arrays.asList(level));
+        }
+        return res;
+    }
+}
